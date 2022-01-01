a5df0368ffe4a6e0c65cc22c1b177db90dcf9238
Juan Patino <juan9patino9@gmail.com>
2022-01-01 14:59:31 -0500

    test screen is now a component

---
+++ components/test.js
@@ -0,0 +1,107 @@
+import React, {useState, useEffect} from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { Image, Text, SafeAreaView, ScrollView, View, StyleSheet, ActivityIndicator, Button
+  , RefreshControl } from 'react-native';
+  import Constants from 'expo-constants';
+import {NavigationContainer} from "@react-navigation/native";
+
+
+export default function test() {
+
+  const[loading, setLoading] = useState(false);
+  const[job, setJob] = useState();
+  const[param, setParam] = useState();
+
+  const loadJob = async() => {
+
+
+    if(!param || !job){
+
+      const res = await fetch('http://jobswipe.tk/?query=SELECT * FROM `Job` WHERE `JobID` = 69');
+
+      const data = await res.json();
+      setJob(data);
+      setLoading(false);
+
+    }
+    else{
+
+      const res = await fetch('http://jobswipe.tk/?query=SELECT * FROM `Job` WHERE ' + param);
+
+      const data = await res.json();
+      setJob(data);
+      setLoading(false);
+
+
+    }
+
+
+
+}
+  
+  useEffect(() =>{
+
+    loadJob();
+
+  },[]);
+
+  if (!job) return (
+
+    <View style = {styles.container}>
+      <Text style={styles.paragraph}>No Results</Text>
+    </View>
+
+  )
+  return (
+    <View style={styles.container}>
+    <SafeAreaView>
+      <ScrollView refreshControl={<RefreshControl refreshing={loading} onRefresh={loadJob}/>}>
+        <Text>{job.Name} - {job.Description}</Text>
+        <Button 
+        title = "search for jobID = 1"
+        onPress={()=>{
+
+          setParam("`JobID` = 1");
+          loadJob();
+
+        }}
+        />
+        <Button 
+        title = "search for jobID = 2"
+        onPress={()=>{
+
+          setParam("`JobID` = 2");
+          loadJob();
+
+        }}
+        />
+      </ScrollView>
+      <Button
+      title = "Set job preferences">
+      </Button>
+    </SafeAreaView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  pic:{
+
+    height: 500,
+    width: 500
+
+  }
+});
