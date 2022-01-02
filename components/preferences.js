b9a7a3f6649e94c7e4d3d5989dc72de158ee5736
Juan Patino <juan9patino9@gmail.com>
2022-01-01 21:44:42 -0500

    preferences page UI created

--- components/preferences.js
+++ components/preferences.js
@@ -1,18 +1,65 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useRef} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Image, Text, SafeAreaView, ScrollView, View, StyleSheet, ActivityIndicator, Button
+import { Image, TextInput, SafeAreaView, ScrollView, View, StyleSheet, ActivityIndicator, Button
   , RefreshControl } from 'react-native';
-  import Constants from 'expo-constants';
-
+import Constants from 'expo-constants';
+import DropDownPicker from 'react-native-dropdown-picker';
 
 export default function preferences({ navigation }) {
+
+    const [open, setOpen] = useState(false);
+    const [value, setValue] = useState(null);
+    const [jobtype, setJobType] = useState([
+      {label: 'Part-Time', value: 'part'},
+      {label: 'Full-Time', value: 'full'},
+      {label: 'Intern/Co-op', value: 'intern'}
+    ]);
+
+
+    const[titleContains, setTitleContains] = useState("");
+    const input = useRef();
 
 
   return (
     <View style={styles.container}>
     <SafeAreaView>
-        <Text>hi</Text>
-    </SafeAreaView>
+    <DropDownPicker
+      style={{
+        backgroundColor: "salmon"
+      }}
+      textStyle={{
+        fontSize: 15
+      }}
+      disabledStyle={{
+        opacity: 0.5
+      }}
+      open={open}
+      value={value}
+      items={jobtype}
+      setOpen={setOpen}
+      setValue={setValue}
+      setItems={setJobType}
+    />
+
+        <TextInput
+        ref={input}
+        style = {styles.txtInput}
+        value={titleContains}
+        onChangeText={setTitleContains}
+        autoCapitalize='none'
+        placeholder='What job? (i.e. title)'    
+    />
+    
+    <Button
+    title="set"
+    onPress={()=>{
+
+        input.current.blur();
+        setTitleContains("");
+        navigation.navigate("test");
+    }}
+    />
+        </SafeAreaView>
     </View>
   );
 }
@@ -20,21 +67,20 @@ export default function preferences({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     paddingTop: Constants.statusBarHeight,
+    alignItems: 'center',
     backgroundColor: '#ecf0f1',
     padding: 8,
   },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-  pic:{
 
-    height: 500,
-    width: 500
+  txtInput:{
+
+   // flex: 1,
+    borderWidth: 2,
+    fontSize: 15,
+    margin: 5,
+    borderRadius: 5,
+    padding: 15
 
   }
 });
